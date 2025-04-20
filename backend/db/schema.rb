06f1935e472c6b17b4dbba89e796d26b8b6d646b
yamato3010 <yama3to10yama3@gmail.com>
2025-04-20 14:55:36 +0900

    feat: ✨ userモデルにnameカラムを追加

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_20_054114) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_20_055310) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_20_054114) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   add_foreign_key "tasks", "users"
