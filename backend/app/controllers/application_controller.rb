ecbbc3a370dee68c8201d4a74d81871c07ec71c9
yamato3010 <yama3to10yama3@gmail.com>
2025-04-20 15:48:47 +0900

    feat: ✨ JWTトークンの無効チェックを追加

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -10,6 +10,10 @@ class ApplicationController < ActionController::API
     header = header.split(' ').last if header
     begin
       @decoded = JsonWebToken.decode(header)
+      if @decoded.nil?
+        render json: { errors: 'Invalid token' }, status: :unauthorized
+        return
+      end
       @current_user = User.find(@decoded[:user_id])
     rescue ActiveRecord::RecordNotFound => e
       render json: { errors: e.message }, status: :unauthorized
