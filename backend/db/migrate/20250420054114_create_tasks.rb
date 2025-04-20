049c880b189773cf025b1ec849203a2c74cf9f9e
yamato3010 <yama3to10yama3@gmail.com>
2025-04-20 14:46:59 +0900

    feat: ✨ taskモデルとuserモデルを追加

---
+++ backend/db/migrate/20250420054114_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[8.0]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :description
+      t.datetime :due_date
+      t.boolean :completed
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
