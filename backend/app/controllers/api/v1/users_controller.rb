ade2591cd0d765503fc6229817866264ae6edd17
yamato3010 <yama3to10yama3@gmail.com>
2025-04-26 16:12:00 +0900

    refactor: ♻️ 記法の修正

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,21 +1,17 @@
 class Api::V1::UsersController < ApplicationController
-  skip_before_action :authorize_request, only: [ :create ]
-  before_action :set_user, only: [ :show, :update, :destroy ]
+  skip_before_action :authorize_request, only: [:create]
+  before_action :set_user, only: [:show, :update, :destroy]
 
   # GET /api/v1/users/:id
   def show
-    render json: @user, status: :ok
+    render json: @user
   end
 
   # POST /api/v1/users
   def create
     @user = User.new(user_params)
     if @user.save
-      token = JsonWebToken.encode(user_id: @user.id)
-      render json: {
-        token: token,
-        user: { id: @user.id, email: @user.email, name: @user.name }
-      }, status: :created
+      render json: @user, status: :created
     else
       render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
     end
