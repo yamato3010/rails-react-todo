3763c906e40931c9f85b51ce4bfda4736ebab2a5
yamato3010 <yama3to10yama3@gmail.com>
2025-04-26 16:28:18 +0900

    feat: ✨ ユーザー作成時にJWTトークンを生成し、レスポンスに含める

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -7,11 +7,18 @@ class Api::V1::UsersController < ApplicationController
     render json: @user
   end
 
-  # POST /api/v1/users
+  # POST /signup
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user, status: :created
+      token = JsonWebToken.encode(user_id: @user.id)
+      time = 24.hours.from_now
+      render json: {
+        token: token,
+        exp: time.strftime("%m-%d-%Y %H:%M"),
+        user_id: @user.id,
+        name: @user.name
+      }, status: :created
     else
       render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
     end
