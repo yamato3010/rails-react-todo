3553e7e716a30ab241990039d915ad4281af196d
yamato3010 <yama3to10yama3@gmail.com>
2025-04-26 16:09:26 +0900

    feat: ✨ 認証情報がないとdashboardを表示できないように

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
+import Logout from './components/Logout';
 
 import './App.css'
 
 function App() {
+  const isAuthenticated = !!localStorage.getItem('token'); // トークンが存在するか確認
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route
+          path="/dashboard"
+          element={isAuthenticated ? <Dashboard /> : <Navigate to="/" />}
+        />
+        <Route path="/logout" element={<Logout />} />
       </Routes>
     </Router>
   );
